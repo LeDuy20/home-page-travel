975a4ed738d676a273090d531c5465edf5ee1b55
LeDuy20 <levanduy02062000@gmail.com>
2024-04-02 10:25:31 +0700

    app.css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import './app.css'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/home'
-import Main from './Components/Main/main'
-import Footer from './Components/Footer/footer'
+import React from 'react';
+import './app.css';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Components/Home/home';
+import Main from './Components/Main/main';
+import Footer from './Components/Footer/footer';
 
 function App() {
-  return (
-      <>
-        <Navbar/>
-        <Home/>
-        <Main/>
-        <Footer/>
-      </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Home />
+            <Main />
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
